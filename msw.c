d070960e12e87ac81e83a5ff04e373023b20913f
Istvan Nagy <isti.nagy03@gmail.com>
2024-10-12 20:53:03 +0200

    Starting the minesweeper project in C for a uni project.

---
+++ msw.c
@@ -0,0 +1,28 @@
+#include <msw.h>
+
+void Init_Board(int x, int y, double diff, Board *board) {
+    board->sizeX = x;
+    board->sizeY = y;
+    board->difficulty = diff;
+    board->cells[board->sizeX][board->sizeY];
+}
+
+void PlaceBombs(Board *board){
+    
+}
+
+int CountNeighbours(Board *board, int x, int y){
+    int result = 0;
+    for (int i = x-1; i <= x+1; i++)
+        for (int j = y-1; j <= y+1; j++)
+            if (i < 0 || j < 0 || i >= board->sizeX || j >= board->sizeY)
+                continue;
+            else
+                if (board->cells[i][j].isBomb)
+                    result++;
+    return result;
+}
+
+void SetNeighbourCount(Board *board, int x, int y, int neighbourCount){
+    board->cells[x][y].neighbours = neighbourCount;
+}
---
+++ msw.h
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+#pragma once
+
+typedef struct Cell{
+    bool isBomb;
+    bool isFlaged;
+    bool isVisited;
+    int neighbours;
+} Cell;
+
+typedef struct Board{
+    Cell **cells;     // Ez lesz maga a tabla
+    int sizeX;
+    int sizeY;
+    double difficulty;    // 0-1 kozott, bombak aranya
+} Board;
+
+void Init_Cell(bool bomb, bool flaged, bool visited, int neighbours);
+void Init_Board(int x, int y, double diff, Board *board);
+void PlaceBombs(Board *board);
+int CountNeighbours(Board *board, int x, int y);
+void SetNeigbourCount(Board *board, int x, int y, int neighbourCount);
+
