dc4c2ca3021bac5175de68195a54875644f14456
István Nagy <59376973+VasutasIsti@users.noreply.github.com>
2024-11-13 20:39:35 +0100

    Update build.bat
    added -g to include debuging informations in the executable

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 ::gcc -m64 -c -Wall -Werror -I.\include .\src\program.c -o minesweeper.exe
 ::echo Build successful!
 
-gcc -I"include" -Wall -Werror -o minesweeper.exe src/*.c
+gcc -g -I"include" -Wall -Werror -o minesweeper.exe src/*.c
